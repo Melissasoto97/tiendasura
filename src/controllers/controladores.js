2d0d7e854819dccc29d998b4b066847bfbdbda55
FelipeVelezG <juanfelipe.velez90@gmail.com>
2022-07-21 17:43:49 -0500

    se modifica el index y se crea controlador

---
+++ src/controllers/controladores.js
@@ -0,0 +1,11 @@
+//CREANDO VARIABLES
+
+let nombreUsuario="Juan Jose"
+const EDAD_USUARIO=33
+let estaturaUsuario=1.62
+let hinchaDelVerde=true
+
+console.log(nombreUsuario)
+
+edadUsuario=20
+console.log(edadUsuario)
