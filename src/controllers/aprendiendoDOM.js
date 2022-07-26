6c4f1bfac7b8c7a1c8bc4abf59a1a8e044509c99
FelipeVelezG <juanfelipe.velez90@gmail.com>
2022-07-26 17:35:24 -0500

    version3

---
+++ src/controllers/aprendiendoDOM.js
@@ -0,0 +1,11 @@
+//PASOS PARA CONTROLAR UNA ETIQUETA DESDDE JS
+
+//1. CONTROLANDO EL CONTENIDO
+let etiquetaTitulo=document.getElementById("titulo1")
+let etiquetaImagen=document.getElementById("foto")
+
+//1.1 manipulando el texto de la etiqueta(elemento)
+etiquetaTitulo.textContent="jefe usted es conmo mi padre"
+
+//1.2 MANIPULANDOI EL SRC
+etiquetaImagen.src="https://pbs.twimg.com/media/FUtIhAUWIAAZfaP.jpg:large"
---
+++ src/controllers/baseDatos.js
@@ -0,0 +1,19 @@
+//QUEMAR (HARD CORE) LA BD
+let productos=[
+
+    {
+        nombre:"FREIDORA AIRE DIGITAL",
+        fotos:["https://firebasestorage.googleapis.com/v0/b/tiendasuramas.appspot.com/o/freidora%20aire%20digital.webp?alt=media&token=6629bbfe-4ed5-4d96-8afe-4c9fa67e0558","https://firebasestorage.googleapis.com/v0/b/tiendasuramas.appspot.com/o/freidora%20aire%20digital1.webp?alt=media&token=b8d4ced7-6070-4c02-be91-b439194e79b4"],
+        precio:799900,
+        popularidad:5,
+        descripcion:""
+    },
+    {
+        nombre:"",
+        fotos:[],
+        precio:0,
+        popularidad:0,
+        descripcion:""
+    }
+
+]
